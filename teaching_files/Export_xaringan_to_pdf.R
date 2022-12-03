6b93c5af4b69d264b84d34d49c0a7cee024ce9d8
Fogelstrom <elsafogelstrom@gmail.com>
2022-12-03 14:00:29 +0100

    minor updates 2022

---
+++ teaching_files/Export_xaringan_to_pdf.R
@@ -0,0 +1,5 @@
+remotes::install_github(c('rstudio/pagedown'))
+install.packages('remotes')
+pagedown::chrome_print('https://fogelstrom.github.io/Elsa/teaching_files/rMarkdown_BiostatIII/IntroToRMarkdown2021.html')
+library(pagedown)
+?chrome_print
